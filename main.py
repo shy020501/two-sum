75aa5ecf6ac52a9bdb09bc9fcea74a5252327e79
shy020501 <shy020501@naver.com>
2023-03-09 20:13:25 +0900

    fixed error

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
 
 def twoSum(self, nums: list[int], target: int) -> list[int]:
-    answer = []
-    for i in range(len(nums)):
-        for j in range(i, len(nums)):
+    answer: list = []
+    for i in range(len(nums)-1):
+        for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
                 answer.append(i)
                 answer.append(j)
